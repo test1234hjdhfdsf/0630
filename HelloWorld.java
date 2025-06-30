9e5e8c8fff4e6e15ed75c82b35ee37c31418f3a4
Cheung0721 <44079148+Cheung0721@users.noreply.github.com>
2025-06-30 14:49:45 +0800

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+  public void main(String[] args) {
+    String token = "dfhjsdf784332";
+    String password = "hello123";
+    System.out.println(token);
+    System.out.println(password);
+  }
+}
