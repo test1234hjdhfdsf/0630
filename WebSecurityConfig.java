7abed357448fa39642b40f5b7a1f3b9fe2c9e2f2
Cheung0721 <44079148+Cheung0721@users.noreply.github.com>
2025-06-30 15:07:19 +0800

    Update WebSecurityConfig.java

--- WebSecurityConfig.java
+++ WebSecurityConfig.java
@@ -14,4 +14,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         csrf.disable() 
       );
   }
+
+  public void evaluate(Socket socket) throws IOException {
+  try (BufferedReader reader = new BufferedReader(
+        new InputStreamReader(socket.getInputStream()))) {
+    
+    String input = reader.readLine();
+    JexlEngine jexl = new JexlBuilder().create();
+    // BAD: input is controlled by the user
+    JexlExpression expression = jexl.createExpression(input);
+    JexlContext context = new MapContext();
+    expression.evaluate(context);
+  }
+}
 }
