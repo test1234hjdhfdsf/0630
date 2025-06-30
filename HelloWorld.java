efae6cc560302e1ac7ef6f67c077004dd1403e68
Cheung0721 <44079148+Cheung0721@users.noreply.github.com>
2025-06-30 17:14:48 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -11,4 +11,16 @@ public class HelloWorld {
             e.printStackTrace();
       }
   }
+
+  public Object evaluate(Socket socket) throws IOException {
+  try (BufferedReader reader = new BufferedReader(
+      new InputStreamReader(socket.getInputStream()))) {
+
+    String string = reader.readLine();
+    ExpressionParser parser = new SpelExpressionParser();
+    // BAD: string is controlled by the user
+    Expression expression = parser.parseExpression(string);
+    return expression.getValue();
+  }
+}
 }
