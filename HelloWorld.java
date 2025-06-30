a3df7b5884123081f2aad808f8c25d76ad34c4ed
Cheung0721 <44079148+Cheung0721@users.noreply.github.com>
2025-06-30 14:44:15 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public Class HelloWorld {
   public void main(String[] args) {
     String token = "hjudsfh438";
     String password = "hello342";
+    String secret = "dfkjdslkf4r0er";
     System.out.println(token);
     System.out.println(password);
   }
